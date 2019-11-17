61584452cffa52780924ec885462a348148d0377
Fernando Britto <me@fernandobritto.com.br>
2019-11-17 14:04:54 -0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+require_once("vendor/autoload.php");
+
+use \Flutter\Page;
+
+$app = new Slim\Slim();
+
+$app->config('debug', true);
+
+$app->get("/", function(){
+    $page = new Page();
+    $page->setTpl("index");
+});
+
+
+$app->run();
+
+
